8811c52c3cd45d813e3a66e0500abe596773701c
kimbsu00 <58127442+kimbsu00@users.noreply.github.com>
2021-05-19 21:51:37 +0900

    MyViewModel is modifed.
    Comments have been added.

--- app/src/main/java/com/mobit/mobit/data/MyViewModel.kt
+++ app/src/main/java/com/mobit/mobit/data/MyViewModel.kt
@@ -5,15 +5,27 @@ import androidx.lifecycle.ViewModel
 
 class MyViewModel : ViewModel() {
 
+    // FragmentCoinList에서 선택한 코인을 다른 Fragment에서 참고할 때 사용하는 변수
+    val selectedCoin: MutableLiveData<CoinInfo> = MutableLiveData()
+
+    // 실시간으로 얻어온 코인 정보를 저장할 변수
     val coinInfo: MutableLiveData<ArrayList<CoinInfo>> = MutableLiveData()
-    val asset: MutableLiveData<ArrayList<Asset>> = MutableLiveData()
+
+    // 사용자가 보유중인 자산 정보를 저장할 변수
+    val asset: MutableLiveData<Asset> = MutableLiveData()
+
+    // 사용자가 매수 또는 매도를 진행할 때마다, 거래 내역을 저장할 변수
     val transaction: MutableLiveData<ArrayList<Transaction>> = MutableLiveData()
+
+    fun setSelectedCoin(selectedCoin: CoinInfo) {
+        this.selectedCoin.value = selectedCoin
+    }
 
     fun setCoinInfo(coinInfo: ArrayList<CoinInfo>) {
         this.coinInfo.value = coinInfo
     }
 
-    fun setAsset(asset: ArrayList<Asset>) {
+    fun setAsset(asset: Asset) {
         this.asset.value = asset
     }
 
